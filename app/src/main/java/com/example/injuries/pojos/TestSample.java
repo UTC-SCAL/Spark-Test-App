ad0611b615dcdf00b24adb1e1a5ce0784b6ce962
Alnour Alharin <alnouralharin@Alnours-MBP.wireless.utc.edu>
2019-02-06 14:51:08 -0500

    added parcel to the samples data

--- app/src/main/java/com/example/injuries/pojos/TestSample.java
+++ app/src/main/java/com/example/injuries/pojos/TestSample.java
@@ -1,8 +1,10 @@
 package com.example.injuries.pojos;
 
+import android.os.Parcel;
+import android.os.Parcelable;
 import android.util.Log;
 
-public class TestSample {
+public class TestSample implements Parcelable {
     private long response_time; //time in msec
     private boolean isResultCorrect = false;
 
@@ -10,6 +12,23 @@ public class TestSample {
         this.response_time = -1;
         this.isResultCorrect = false;
     }
+
+    protected TestSample(Parcel in) {
+        response_time = in.readLong();
+        isResultCorrect = in.readByte() != 0;
+    }
+
+    public static final Creator<TestSample> CREATOR = new Creator<TestSample>() {
+        @Override
+        public TestSample createFromParcel(Parcel in) {
+            return new TestSample(in);
+        }
+
+        @Override
+        public TestSample[] newArray(int size) {
+            return new TestSample[size];
+        }
+    };
 
     public long getResponse_time() {
         return response_time;
@@ -29,5 +48,16 @@ public class TestSample {
 
     public void showInfo() {
         Log.i("test_sample", "(time, res) = (" + response_time + ", " + isResultCorrect + ")");
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeLong(response_time);
+        dest.writeByte((byte) (isResultCorrect? 1:0));
     }
 }
--- app/src/main/java/com/example/injuries/pojos/TestSamplesContainer.java
+++ app/src/main/java/com/example/injuries/pojos/TestSamplesContainer.java
@@ -1,18 +1,37 @@
 package com.example.injuries.pojos;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
-public class TestSamplesContainer implements Iterable<TestSample>{
+public class TestSamplesContainer implements Iterable<TestSample>, Parcelable {
     private List<TestSample> testSamples;
 
     public TestSamplesContainer(int maxSize) {
         this.testSamples = new ArrayList<>(maxSize);
         for(int i = 0 ; i < maxSize; i++){
-            testSamples.set(i, new TestSample());
+            testSamples.add(i, new TestSample());
         }
     }
+
+    protected TestSamplesContainer(Parcel in) {
+        testSamples = in.createTypedArrayList(TestSample.CREATOR);
+    }
+
+    public static final Creator<TestSamplesContainer> CREATOR = new Creator<TestSamplesContainer>() {
+        @Override
+        public TestSamplesContainer createFromParcel(Parcel in) {
+            return new TestSamplesContainer(in);
+        }
+
+        @Override
+        public TestSamplesContainer[] newArray(int size) {
+            return new TestSamplesContainer[size];
+        }
+    };
 
     public void setResponseTime(int index, long response_time){
         testSamples.get(index).setResponse_time(response_time);
@@ -25,5 +44,15 @@ public class TestSamplesContainer implements Iterable<TestSample>{
     @Override
     public Iterator<TestSample> iterator() {
         return testSamples.iterator();
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeList(testSamples);
     }
 }
