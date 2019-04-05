c71424a9d16c367c802b6c2c6a3c594d1e68ce87
Alnour Alharin <alnour.ahmed@gmail.com>
2019-04-05 01:11:42 -0400

    used the server IP

--- app/src/main/java/com/example/injuries/apis/API_Constants.java
+++ app/src/main/java/com/example/injuries/apis/API_Constants.java
@@ -1,5 +1,5 @@
 package com.example.injuries.apis;
 
 public class API_Constants {
-    public static String BASE_URL = "http:/192.168.1.7:8000";
+    public static String BASE_URL = "http://sparkerserver.research.utc.edu:8000";
 }
