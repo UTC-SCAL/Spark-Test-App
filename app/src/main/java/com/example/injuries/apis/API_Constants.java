077e57c5a3d78806ee49113df084bb0e2fb8e7a2
Alnour Alharin <alnour.ahmed@gmail.com>
2020-08-29 10:07:43 -0400

    server migration

--- app/src/main/java/com/example/injuries/apis/API_Constants.java
+++ app/src/main/java/com/example/injuries/apis/API_Constants.java
@@ -1,6 +1,6 @@
 package com.example.injuries.apis;
 
 public class API_Constants {
-    public static String BASE_URL = "http://sparkerserver.research.utc.edu:8000";
-//    public static String BASE_URL = "http://192.168.1.129:8000";
+//    public static String BASE_URL = "http://sparkerserver.research.utc.edu:8000";
+    public static String BASE_URL = "http://3.128.94.184:8080";
 }
